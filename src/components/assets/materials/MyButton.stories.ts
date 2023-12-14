63db05d2d80011ab59c9d89cfeded0ffdc31c3dd
Takeda Takumi <56464951+TakedaTakumi@users.noreply.github.com>
2023-12-14 15:20:16 +0000

    feat: add backgrounds addon

--- src/components/assets/materials/MyButton.stories.ts
+++ src/components/assets/materials/MyButton.stories.ts
@@ -4,52 +4,74 @@ import type { Meta, StoryObj } from "@storybook/vue3";
 type Story = StoryObj<typeof MyButton>;
 
 const meta: Meta<typeof MyButton> = {
-  title: "Assets/Material/MyButton",
-  component: MyButton,
-  render: (args) => ({
-    components: { MyButton },
-    setup() {
-      return { args };
-    },
-    template: "<MyButton v-bind='args' />",
-  }),
-  args: {
-    label: "ボタン",
-    variant: "primary",
-    size: "medium",
-  },
-  argTypes: {
-    variant: {
-      control: {
-        type: "inline-radio",
-      },
-      options: ["primary", "secondary"],
-    },
-    size: {
-      control: {
-        type: "select",
-      },
-      options: ["small", "medium", "large"],
-    },
-  },
+	title: "Assets/Material/MyButton",
+	component: MyButton,
+	render: (args) => ({
+		components: { MyButton },
+		setup() {
+			return { args };
+		},
+		template: "<MyButton v-bind='args' />",
+	}),
+	args: {
+		label: "ボタン",
+		variant: "primary",
+		size: "medium",
+	},
+	argTypes: {
+		variant: {
+			control: {
+				type: "inline-radio",
+			},
+			options: ["primary", "secondary"],
+		},
+		size: {
+			control: {
+				type: "select",
+			},
+			options: ["small", "medium", "large"],
+		},
+	},
+	parameters: {
+		backgrounds: {
+			default: "twitter",
+			values: [
+				{
+					name: "twitter",
+					value: "#00aced",
+				},
+				{
+					name: "facebook",
+					value: "#3b5998",
+				},
+			],
+		},
+	},
 };
 
 export const Default: Story = {
-  args: {
-    label: "ボタン",
-  },
+	args: {
+		label: "ボタン",
+		variant: "primary",
+		size: "medium",
+	},
+	parameters: {
+		backgrounds: {
+			default: "twitter",
+		},
+	},
 };
 
 export const Login: Story = {
-  args: {
-    label: "ログイン",
-  },
+	args: {
+		label: "ログイン",
+	},
 };
 
 export const SignUp: Story = {
-  args: {
-    label: "会員登録",
-  },
+	args: {
+		label: "会員登録",
+	},
 };
 
 export default meta;
