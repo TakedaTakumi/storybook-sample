aaf20aa5bc182243aa541bab5fc848517ba8297b
Takeda Takumi <56464951+TakedaTakumi@users.noreply.github.com>
2023-12-15 14:32:06 +0000

    feat: update decorator

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,29 +1,37 @@
 // cSpell:word viewports
+import { Decorator } from "@storybook/vue3";
 export const parameters = {
-  // Viewport addon で使用される設定
-  viewport: {
-    // プリセットをアプリケーションの仕様に合わせて定義
-    viewports: {
-      pc: {
-        name: "Min PC Layout",
-        styles: {
-          width: "992px",
-          height: "100%",
-        },
-      },
-      mobile: {
-        name: "Min Mobile Layout",
-        styles: {
-          width: "375px",
-          height: "100%",
-        },
-      },
-    },
-    // すべてのストーリーでデフォルト PC ビューを使用する
-    defaultViewport: "pc",
-  },
+	// Viewport addon で使用される設定
+	viewport: {
+		// プリセットをアプリケーションの仕様に合わせて定義
+		viewports: {
+			pc: {
+				name: "Min PC Layout",
+				styles: {
+					width: "992px",
+					height: "100%",
+				},
+			},
+			mobile: {
+				name: "Min Mobile Layout",
+				styles: {
+					width: "375px",
+					height: "100%",
+				},
+			},
+		},
+		// すべてのストーリーでデフォルト PC ビューを使用する
+		defaultViewport: "pc",
+	},
 };
 
-export const decorators = [];
+export const decorators: Decorator[] = [
+	() => {
+		return {
+			template:
+				'<div style="margin: 5em; border: 1px solid; border-color: white"><story /></div>',
+		};
+	},
+];
 
 export const globalTypes = {};
