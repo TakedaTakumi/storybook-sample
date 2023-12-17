5c5fa46766efe3edddb983a721fab8e2da5b3b94
Takeda Takumi <56464951+TakedaTakumi@users.noreply.github.com>
2023-12-17 15:07:44 +0000

    feat: add i18n addon

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,32 @@
-import { Decorator, Parameters } from "@storybook/vue3";
+import { Decorator, Parameters, setup } from "@storybook/vue3";
+import i18n from "../src/i18n";
+
 export const parameters: Parameters = {};
 
-export const decorators: Decorator[] = [];
+export const decorators: Decorator[] = [
+	(story, context) => {
+		i18n.global.locale = context.globals.locale;
+		return { template: "<story />" };
+	},
+];
+
+export const globalTypes = {
+	locale: {
+		name: "Locale",
+		description: "多言語対応",
+		defaultValue: "ja",
+		toolbar: {
+			icon: "globe",
+			items: [
+				{ value: "ja", right: "🇯🇵", title: "Japanese" },
+				{ value: "en", right: "🇺🇸", title: "English" },
+			],
+		},
+	},
+};
+
+setup((app) => {
+	if (!app.__VUE_I18N__) {
+		app.use(i18n);
+	}
+});
